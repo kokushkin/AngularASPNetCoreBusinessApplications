7dce1a0bc5c22d991b8871f4f6a40bc3f195336c
kokushkin <fipsiksi@yandex.ru>
2019-03-02 23:53:10 +0200

    Server side validation "Description" (before: 6 Summary)

--- Starter files/TourManagement.API/Dtos/TourAbstractBase.cs
+++ Starter files/TourManagement.API/Dtos/TourAbstractBase.cs
@@ -9,7 +9,7 @@ namespace TourManagement.API.Dtos
         [Required(AllowEmptyStrings = false, ErrorMessage = "required|Title is required.")]
         [MaxLength(200, ErrorMessage = "maxLength|Title is too long.")]
         public string Title { get; set; }
-        public string Description { get; set; }
+        public virtual string Description { get; set; }
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
 
--- Starter files/TourManagement.API/Dtos/TourForUpdate.cs
+++ Starter files/TourManagement.API/Dtos/TourForUpdate.cs
@@ -8,9 +8,9 @@ namespace TourManagement.API.Dtos
 {
     public class TourForUpdate : TourAbstractBase
     {
-        //[Required(AllowEmptyStrings = false, 
-        //    ErrorMessage = "required|When updating a tour, the description is required.")]
-        //public override string Description
-        //{ get => base.Description; set => base.Description = value; }
+        [Required(AllowEmptyStrings = false,
+            ErrorMessage = "When updating a tour, the description is required.")]
+        public override string Description
+        { get => base.Description; set => base.Description = value; }
     }
 }
