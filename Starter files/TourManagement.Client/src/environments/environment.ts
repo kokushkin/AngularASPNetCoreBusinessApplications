369e6f7331fac8af8e7be686ffcb8e09caa494c5
kokushkin <fipsiksi@yandex.ru>
2019-03-25 23:36:28 +0200

    Some fixes. Now all good. Before: 10: Working with authorization Policies

--- Starter files/TourManagement.Client/src/environments/environment.ts
+++ Starter files/TourManagement.Client/src/environments/environment.ts
@@ -12,8 +12,8 @@ export const environment = {
     redirect_uri: 'https://localhost:4200/signin-oidc',
     scope: 'openid profile roles tourmanagementapi',
     response_type: 'id_token token',
-    post_logout_direct_uri: 'https://localhost:4200/',
-    autmaticSilentRenew: true,
+    post_logout_redirect_uri: 'https://localhost:4200/',
+    autmaticSilentRenew: true,    
     silent_redirect_uri: 'https://localhost:4200/redirect-silentrenew'
   }
 };
